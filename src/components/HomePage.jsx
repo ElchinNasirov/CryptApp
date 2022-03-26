6a4688e9745b8154cbbef0e11a283ccf602a3936
ElchinNasirov <nasirov.elchin@gmail.com>
2022-03-26 09:54:23 -0700

    Adding Redux store.

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,35 @@
-import { Typography } from "antd";
+import { Typography, Row, Col, Statistic } from "antd";
 import React from "react";
 
 const HomePage = () => {
   return (
     <>
       <Typography.Title level={2} className="heading">
-        {" "}
         Crypto Stats
       </Typography.Title>
+
+      <Row>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Cryptocurrencies" value={1} />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Exchanges" value={1} />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Market Cap" value={1} />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total 24h Volume" value={1} />{" "}
+        </Col>
+        <Col span={12}>
+          {" "}
+          <Statistic title="Total Markets" value={1} />{" "}
+        </Col>
+      </Row>
     </>
   );
 };
