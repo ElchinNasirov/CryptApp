6528e947bb87d11b76f41ff41b469a7fc0e8f8eb
ElchinNasirov <nasirov.elchin@gmail.com>
2022-04-11 21:12:31 -0700

    Working on CryptoDetails.jsx to show selected crypto information, not finished.

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { Col, Typography, Select } from "antd";
+import millify from "millify";
+import { DollarCircleOutlined, TrophyOutlined, NumberOutlined, ThunderboltOutlined } from "@ant-design/icons";
 
 import { useGetCryptoDetailsQuery } from "../services/cryptoAPI";
 
@@ -13,6 +15,14 @@ const CryptoDetails = () => {
   const cryptoDetails = data?.data?.coin;
 
   const time = ["3h", "24h", "7d", "30d", "1y", "3m", "3y", "5y"];
+
+  const stats = [
+    { title: "Price to USD", value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
+    { title: "Rank", value: cryptoDetails?.rank, icon: <NumberOutlined /> },
+    { title: "24h Volume", value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`, icon: <ThunderboltOutlined /> },
+    { title: "Market Cap", value: `$ ${cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
+    { title: "All-time-high(daily avg.)", value: `$ ${cryptoDetails?.allTimeHigh?.price && millify(cryptoDetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
+  ];
 
   return (
     <Col className="coin-detail-container">
@@ -27,6 +37,25 @@ const CryptoDetails = () => {
           <Option key={date}>{date}</Option>
         ))}
       </Select>
+      <Col className="stats-container">
+        <Col className="coin-value-statistics">
+          <Col className="coin-value-statistics-heading">
+            <Typography.Title level={3} className="coin-details-heading">
+              {cryptoDetails.name} Value Statistics
+            </Typography.Title>
+            <p>An Overview showing the stats of Bitcoin</p>
+          </Col>
+          {stats.map(({ icon, title, value }) => (
+            <Col className="coin-stats">
+              <Col className="coin-stats-name">
+                <Typography.Text>{icon}</Typography.Text>
+                <Typography.Text>{title}</Typography.Text>
+              </Col>
+              <Typography.Text className="stats">{value}</Typography.Text>
+            </Col>
+          ))}
+        </Col>
+      </Col>
     </Col>
   );
 };
